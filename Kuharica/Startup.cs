1d2745ea0680085ce21803d9d1fea9ccdb953a22
yeahnyea <ipotocki22@gmail.com>
2017-11-27 17:37:51 +0100

    Add project files.

---
+++ Kuharica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kuharica.Startup))]
+namespace Kuharica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
