e76f64c4f88a2d4724afe9cee8ac1123001e75d7
yeahnyea <ipotocki22@gmail.com>
2017-11-28 16:06:44 +0100

    Create Add a Recipe form.

---
+++ Kuharica/Controllers/RecipesController.cs
@@ -0,0 +1,28 @@
+﻿using Kuharica.Models;
+using System.Linq;
+using System.Web.Mvc;
+using Kuharica.ViewModels;
+
+namespace Kuharica.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecipesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+       
+        // GET: Recipes
+        public ActionResult Create()
+        {
+            var viewModel = new RecipeFormViewModel
+            {
+                Meals = _context.Meals.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Kuharica/ViewModels/RecipeFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Kuharica.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kuharica.ViewModels
+{
+    public class RecipeFormViewModel
+    {
+        public byte Id { get; set; }
+
+        [Display(Name = "Vrijeme pripreme")]
+        public string Time { get; set; }
+
+        [Display(Name = "Ime i prezime")]
+        public string Name { get; set; }
+
+        // For dropdown numeric values
+        [Display(Name = "Vrsta jela" )]
+        public int Meal { get; set; }
+        public IEnumerable<Meal> Meals { get; set; }
+    }
+}
